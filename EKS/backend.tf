66072da728fb694efcbbc80e1321274801837d03
Chytragt <152904883+Chytragt@users.noreply.github.com>
2024-11-30 23:07:35 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amazon-s3-bucket-project1"  # Replace with your actual S3 bucket name
+    bucket = "my-eks-project-new-bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "us-east-1" 
   }
